28ebc291d66cc7982bd16c2c7143733b3ff7fb20
Vel <meisterrabyte@gmail.com>
2025-04-04 19:33:30 +0800

    Add a Product and Inventory class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_STOCK = 50;
+const int MAX_INV = 50;
+
+class Product
+{
+private:
+    string prodName = "";
+    int stock = 0;
+    double price = 0;
+
+    friend class Inventory;
+};
+class Inventory
+{
+    Product prodList[MAX_INV];
+    int numProd = 0;
+};
+
 int main()
 {
 }
