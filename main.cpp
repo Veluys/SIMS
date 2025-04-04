18ed6097dc075f343a6091e6c724a49c2b9c219c
Vel <meisterrabyte@gmail.com>
2025-04-04 20:07:56 +0800

    Add an addProd function and display function to the Inventory

--- main.cpp
+++ main.cpp
@@ -11,14 +11,49 @@ private:
     int stock = 0;
     double price = 0;
 
+    void display() const
+    {
+        cout << "\n"
+             << "Product Name: " << prodName << endl;
+        cout << "Stock: " << stock << endl;
+        cout << "Price: " << price << endl;
+        cout << "-------------------------------" << endl;
+    }
+
     friend class Inventory;
 };
+
 class Inventory
 {
     Product prodList[MAX_INV];
     int numProd = 0;
+
+public:
+    void addProd()
+    {
+        Product *newProd = &prodList[numProd];
+
+        cout << "Enter product name: ";
+        getline(cin, newProd->prodName);
+
+        cout << "Enter initial stock: ";
+        cin >> newProd->stock;
+
+        cout << "Enter initial price: ";
+        cin >> newProd->price;
+
+        numProd++;
+    }
+    void showInv() const
+    {
+        for (size_t i = 0; i < numProd; i++)
+        {
+            prodList[i].display();
+        }
+    }
 };
 
 int main()
 {
+    return 0;
 }
